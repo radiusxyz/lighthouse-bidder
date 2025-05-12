0caee20902a6eb08e4bf21183f0d488b372a4fa1
k930503 <k930503@gmail.com>
2025-05-12 10:21:34 +0900

    chore

--- txbuilder/tx_builder.go
+++ txbuilder/tx_builder.go
@@ -11,6 +11,7 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 	"math/big"
+	"strings"
 )
 
 type TxBuilder struct {
@@ -30,12 +31,11 @@ func New(url string) (*TxBuilder, error) {
 }
 
 func (t *TxBuilder) GetSignedTransaction(privateKeyHex string, toAddress common.Address) (string, error) {
-	privateKey, err := crypto.HexToECDSA(privateKeyHex)
+	privateKey, err := crypto.HexToECDSA(strings.TrimPrefix(privateKeyHex, "0x"))
 	if err != nil {
 		log.Fatalf("invalid private key: %v", err)
 	}
 
-	// 3. 주소, nonce 조회
 	publicKey := privateKey.Public().(*ecdsa.PublicKey)
 	fromAddress := crypto.PubkeyToAddress(*publicKey)
 
@@ -44,7 +44,6 @@ func (t *TxBuilder) GetSignedTransaction(privateKeyHex string, toAddress common.
 		log.Fatalf("failed to get nonce: %v", err)
 	}
 
-	// 4. 트랜잭션 파라미터 설정
 	value := big.NewInt(10000000000000000)                             // 0.01 ETH
 	gasLimit := uint64(21000)                                          // 기본 전송
 	gasPrice, err := t.ethClient.SuggestGasPrice(context.Background()) // legacy tx
@@ -52,22 +51,18 @@ func (t *TxBuilder) GetSignedTransaction(privateKeyHex string, toAddress common.
 		log.Fatalf("failed to suggest gas price: %v", err)
 	}
 
-	// 5. 트랜잭션 생성 (legacy 트랜잭션 예시)
 	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, nil)
 
-	// 6. 체인 ID 확인 (ex: 1=mainnet, 5=Goerli, 1337=anvil)
 	chainID, err := t.ethClient.NetworkID(context.Background())
 	if err != nil {
 		log.Fatalf("failed to get chain ID: %v", err)
 	}
 
-	// 7. 서명 및 RLP 인코딩
 	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
 	if err != nil {
 		log.Fatalf("failed to sign tx: %v", err)
 	}
 
-	// 8. rawTransaction 생성
 	rawTxBytes, err := signedTx.MarshalBinary()
 	if err != nil {
 		log.Fatalf("failed to encode tx: %v", err)
